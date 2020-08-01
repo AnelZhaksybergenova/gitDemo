161492013817566e030dbd7f24e8e5e6c2a5594e
AnelZhaksybergenova <anelbolatbek1@gmail.com>
2020-08-01 11:00:42 -0500

    update Happy20

--- src/Happy20.java
+++ src/Happy20.java
@@ -1,6 +1,7 @@
 public class Happy20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
+        System.out.println("Hello, Cybertek School");
     }
 
 }
